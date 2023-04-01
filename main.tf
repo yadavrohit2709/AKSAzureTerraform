c4d9d181702454003e984fae17b86b8032a1a51c
yadavrohit2709 <118057903+yadavrohit2709@users.noreply.github.com>
2023-04-01 12:45:01 +0530

    modified resource group to put into data section

--- main.tf
+++ main.tf
@@ -17,6 +17,11 @@ data "azurerm_key_vault_secret" "spn_secret" {
   key_vault_id = data.azurerm_key_vault.azure_vault.id
 }
 
+data "azurerm_resource_group" "aks_rg" {
+  name     = var.resource_group
+  location = var.azure_region
+}
+
 resource "azurerm_virtual_network" "aks_vnet" {
   name                = var.aks_vnet_name
   resource_group_name = azurerm_resource_group.aks_rg.name
@@ -32,10 +37,6 @@ resource "azurerm_subnet" "aks_subnet" {
 }
 
 
-resource "azurerm_resource_group" "aks_rg" {
-  name     = var.resource_group
-  location = var.azure_region
-}
 
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = var.cluster_name
