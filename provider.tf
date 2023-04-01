a136572f24e0a77e028797f8d705a1e1543ec00d
yadavrohit2709 <118057903+yadavrohit2709@users.noreply.github.com>
2023-04-01 16:27:06 +0530

    updated backend variable names

--- provider.tf
+++ provider.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }  
   backend "azurerm" {
-    resource_group_name = var.bkstrgrg
-    storage_account_name = var.bkstrg
-    container_name = var.bkcontainer
-    key = var.bkstrgkey
+    resource_group_name = var.BKSTRGRG
+    storage_account_name = var.BKSTRG
+    container_name = var.BKCONTAINER
+    key = var.BKSTREGKEY
   }
 }
 provider "azurerm" {
