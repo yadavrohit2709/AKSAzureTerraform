1f95572133fca105eebdb0455c1eb872d7d76cd5
yadavrohit2709 <118057903+yadavrohit2709@users.noreply.github.com>
2023-04-01 12:55:17 +0530

    added backend conifguration in provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,16 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
+      source  = "hashicorp/azurerm"
+      version = "3.15.00"
     }
+  }  
+  backend "azurerm" {
+    resource_group_name = var.bkstrgrg
+    storage_account_name = var.bkstrg
+    container_name = var.bkcontainer
+    key = var.bkstrgkey
   }
-
 }
 provider "azurerm" {
   features {}
