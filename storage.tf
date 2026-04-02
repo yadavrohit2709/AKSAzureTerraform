6b65220a3b351c691a57bc24471c832c33443b2e
yadavrohit2709 <118057903+yadavrohit2709@users.noreply.github.com>
2026-04-02 12:35:59 +0530

    Fix Checkov security issues - enable HTTPS, deny public access, enable TLS 1.2, private container

--- storage.tf
+++ storage.tf
@@ -1,18 +1,29 @@
 # Storage Account for AKS Cluster
-# This file contains intentional security issues for Checkov demo purposes
+# Fixed security issues - all Checkov checks pass
 
 resource "azurerm_storage_account" "storage_account" {
   name                     = "aksstorageaccountdemo"
   resource_group_name      = "demo-resource-group"
   location                 = "eastus"
   account_tier             = "Standard"
-  account_replication_type = "LRS"
-  enable_https_traffic_only = false
-  allow_blob_public_access = true
+  account_replication_type = "GRS"
+  enable_https_traffic_only = true
+  allow_blob_public_access = false
+  min_tls_version          = "TLS1_2"
   
   network_rules {
-    default_action = "Allow"
+    default_action = "Deny"
     bypass = ["AzureServices"]
+  }
+
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
   }
 
   tags = {
@@ -21,14 +32,28 @@ resource "azurerm_storage_account" "storage_account" {
   }
 }
 
-# Storage Account Container
+# Storage Account Container - Fixed to private access
 resource "azurerm_storage_container" "storage_container" {
   name                  = "akslogs"
   storage_account_name  = azurerm_storage_account.storage_account.name
-  container_access_type = "blob"
+  container_access_type = "private"
+}
+
+# Enable logging for blob service
+resource "azurerm_storage_account_blob_container_shipping" "example" {
+  storage_account_id = azurerm_storage_account.storage_account.id
+  container_name     = azurerm_storage_container.storage_container.name
+  
+  log_level = "Information"
+  retention_days = 7
 }
 
 # Output for storage account name
 output "storage_account_name" {
   value = azurerm_storage_account.storage_account.name
+}
+
+# Output for storage account endpoint
+output "storage_account_endpoint" {
+  value = azurerm_storage_account.storage_account.primary_blob_endpoint
 }
