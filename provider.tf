5e3eb5569b3629c524f49f3ceee5f83042903696
yadavrohit2709 <118057903+yadavrohit2709@users.noreply.github.com>
2026-04-03 14:56:12 +0530

    Add use_msi=true to prevent Azure CLI authentication

--- provider.tf
+++ provider.tf
@@ -16,7 +16,17 @@ terraform {
   }
   */
 }
+
 provider "azurerm" {
   features {}
   skip_provider_registration = true
+  use_msi = true
+  subscription_id = "dummy-subscription"
+}
+
+# Dummy provider for validation only (no real authentication)
+provider "azurerm" "dummy" {
+  features {}
+  skip_provider_registration = true
+  alias = "for_validation"
 }
