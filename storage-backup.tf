8200e385362d865a239f225f2033d9da53768b37
yadavrohit2709 <118057903+yadavrohit2709@users.noreply.github.com>
2026-04-04 14:30:49 +0530

    Add storage backup configuration for AKS cluster

---
+++ storage-backup.tf
@@ -0,0 +1,46 @@
+# Storage Account for AKS Cluster
+# Backup configuration - Updated for new deployment requirements
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "aksstorageaccountdemo"
+  resource_group_name      = "demo-resource-group"
+  location                 = "eastus"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  
+  # VULNERABILITY: Public access enabled - needs to be disabled for production
+  allow_blob_public_access = true
+  
+  # VULNERABILITY: Weak TLS version - should use TLS1_2
+  min_tls_version          = "TLS1_0"
+  
+  # VULNERABILITY: Network rules allow public access
+  network_rules {
+    default_action = "Allow"
+  }
+  
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
+
+  tags = {
+    Environment = "Demo"
+    Project = "AKS Terraform Demo"
+  }
+}
+
+resource "azurerm_storage_container" "storage_container" {
+  name                  = "akslogs"
+  storage_account_name = azurerm_storage_account.storage_account.name
+  container_access_type = "blob"  # VULNERABILITY: Public blob access
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage_account.name
+}
+
+output "storage_account_endpoint" {
+  value = azurerm_storage_account.storage_account.primary_blob_endpoint
+}
