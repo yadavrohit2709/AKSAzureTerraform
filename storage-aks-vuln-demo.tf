17f21ddd45d42a331cd6eb032f202d4e6836f958
yadavrohit2709 <118057903+yadavrohit2709@users.noreply.github.com>
2026-04-08 17:23:18 +0530

    Add vulnerable storage-aks-vuln-demo.tf with Terraform-compatible syntax

---
+++ storage-aks-vuln-demo.tf
@@ -0,0 +1,44 @@
+# Storage Account for AKS Cluster - DEMO WITH VULNERABILITIES
+# This file contains intentional security vulnerabilities for demonstration
+# Compatible with Azure Provider 3.15.00
+
+resource "azurerm_storage_account" "aks_demo_vuln_storage" {
+  name                     = "aksdemovuln001"
+  resource_group_name      = "demo-resource-group"
+  location                 = "eastus"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  
+  # VULNERABILITY: Weak TLS version - should use TLS1_2
+  min_tls_version          = "TLS1_0"
+  
+  # VULNERABILITY: Network rules allow public access
+  network_rules {
+    default_action = "Allow"
+  }
+  
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
+
+  tags = {
+    Environment = "Demo"
+    Project = "AKS Terraform Demo"
+  }
+}
+
+resource "azurerm_storage_container" "aks_demo_vuln_container" {
+  name                  = "akslogsvuln"
+  storage_account_name = azurerm_storage_account.aks_demo_vuln_storage.name
+  container_access_type = "blob"  # VULNERABILITY: Public blob access
+}
+
+output "aks_demo_vuln_storage_account_name" {
+  value = azurerm_storage_account.aks_demo_vuln_storage.name
+}
+
+output "aks_demo_vuln_storage_account_endpoint" {
+  value = azurerm_storage_account.aks_demo_vuln_storage.primary_blob_endpoint
+}
